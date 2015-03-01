7c6a1172f051343632e17aa6aa9406784330c8c6
rlyshw <rlyshw@gmail.com>
2015-03-01 12:58:59 -0500

    tidied up Deck a little more

--- PokerEngine/src/pokar/Deck.java
+++ PokerEngine/src/pokar/Deck.java
@@ -11,14 +11,6 @@ public class Deck {
 	}
 
 	public Deck() {
-		// Todo: generate deck of 52 cards
-		/* Commented out because this is assuming cardArray is an array not an arraylist
-		 *  for (int i = 1; i <= 13; i++) {
-			this.cardArray[i - 1] = new Card(i, 'S');
-			this.cardArray[i + 12] = new Card(i, 'D');
-			this.cardArray[i + 25] = new Card(i, 'H');
-			this.cardArray[i + 38] = new Card(i, 'C');
-		}*/
 		for(int j = 0; j<=4; j++){
 			Suit suitRank = Suit.values()[j];
 			for(int i =0; i<14; i++){
