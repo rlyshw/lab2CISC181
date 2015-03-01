5ab6f68567d41c202d0d6ea8ac2eeda174f1de2a
Tommy Kananen <tkananen@udel.edu>
2015-03-01 12:34:19 -0500

    suitenum

--- PokerEngine/src/pokar/Card.java
+++ PokerEngine/src/pokar/Card.java
@@ -32,4 +32,5 @@ public class Card {
 	public boolean isAvail() {
 		return true;
 	}
+	
 }
--- PokerEngine/src/pokar/Suit.java
+++ PokerEngine/src/pokar/Suit.java
@@ -2,8 +2,17 @@ package pokar;
 
 public enum Suit {
 	//enum is like a dictionary but it's a class or something
-	Hearts,
-	Club,
-	Diamond,
-	Spade;
+	CLUBS(0),
+	DIAMONDS(1),
+	HEARTS(2),
+	SPADES(3);
+	
+	private int suitRank;
+	
+	private Suit(int value){
+		this.suitRank = value;
+	}
+	public int getsuitRank(){
+		return suitRank;
+	}
 }
